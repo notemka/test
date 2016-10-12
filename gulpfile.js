28fa79514d2021117fb774dd90616389db954f44
notemka <emma.saitgaraeva@gmail.com>
2016-10-13 00:06:01 +0400

    gulpfile refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,12 @@
 var browserify = require('browserify');
 var gulp = require("gulp");
 var source = require('vinyl-source-stream');
-var useref = require("gulp-useref");
-var gulpif = require("gulp-if");
-var rename = require("gulp-rename");
+var buffer = require("vinyl-buffer");
 var cssnano = require("cssnano");
 
 var babelify = require("babelify");
 
-var postcss = require("gulp-postcss");        // npm install --save-dev gulp-postcss
+var postcss = require("gulp-postcss");
 var vars = require("postcss-simple-vars");
 var autoprefixer = require("autoprefixer");
 var cssnext = require("cssnext");             // включает синтаксис будущего
@@ -33,32 +31,20 @@ var imagemin = require("gulp-imagemin");   //минификация изобра
 var browserSync = require("browser-sync");  //для обновления страницы при разработке
 var reload = browserSync.reload;
 
-      // ============
-      // == Сборка ==
-      // ============
-
 // очистка папки
 gulp.task("clean", function () {
-  return gulp.src("dest")
+  return gulp.src("./dest/*")
     .pipe(clean());
-});
-
-// перенос html, css, js в папку dest
-gulp.task("useref", function () {
-  return gulp.src("app/*.html")
-    .pipe(gulpif("app/js/*.js", uglify()))
-    .pipe(useref())
-    .pipe(gulp.dest("dest")); // прописывает новые пути
 });
 
 // перенос шрифтов
 gulp.task("fonts", function(){
-  gulp.src("./app/fonts/*")
+  return gulp.src("./app/fonts/*")
     .pipe(filter(["*.eot", "*.svg", "*.ttf", "*.woff", "*.woff2"]))
     .pipe(gulp.dest("./dest/fonts"));
 });
 
-// картинки
+// оптимизация и перенос картинок
 gulp.task("images", function () {
   return gulp.src("./app/img/*")
     .pipe(imagemin({
@@ -68,34 +54,23 @@ gulp.task("images", function () {
     .pipe(gulp.dest("./dest/img"));
 });
 
-// остальные файлы, такие как favicon и пр.
+// перенос остальных файлов, такие как favicon и пр.
 gulp.task("extras", function () {
-  return gulp.src([
-    "./app/*.*",
-    "!app/*.html"
-  ]).pipe(gulp.dest("./dest"));
+  return gulp.src(["./app/*.*", "!app/*.html"])
+              .pipe(gulp.dest("./dest"));
 });
 
-// сборка и вывод размера содержимого папки dest
-gulp.task("dest", ["useref", "images", "fonts", "extras"], function(){
-  return gulp.src("./dest/**/*").pipe(size({title: "build"}));
-});
-
-      // ==================
-      // == Работа с APP ==
-      // ==================
-
-// Компилируем Jade в html
+// компиляция Jade в html
 gulp.task("jade", function() {
-  gulp.src("./app/templates/pages/*.jade")
+  return gulp.src("./app/templates/pages/*.jade")
     .pipe(jade())
     .on("error", log)
     .pipe(prettify({indent_size: 2}))
-    .pipe(gulp.dest("./app"))
+    .pipe(gulp.dest("./dest"))
     .pipe(reload({stream: true}));
 });
 
-// Компилируем css
+// компиляция css
 gulp.task("css", function() {
   var processors = [
     stylelint(config),
@@ -112,51 +87,57 @@ gulp.task("css", function() {
     cssnano()
   ];
 
-  return gulp.src("app/css/main.css")
+  return gulp.src("./app/css/styles.css")
     .pipe(postcss(processors))
     .on("error", log)
-    .pipe(rename("bundle.css"))
-    .pipe(gulp.dest("app/css"))
+    .pipe(gulp.dest("./dest/css"))
     .pipe(reload({stream: true}));
 });
 
-// Компилируем js
+// компиляция js
 gulp.task("js", function() {
   return browserify({entries: "./app/js/main.js", debug: true})
     .transform("babelify", {presets: ["es2015"]})
     .bundle()
-    // .pipe(uglify())
     .pipe(source("bundle.js"))
+    .pipe(buffer())
+    .pipe(uglify())
     .on("error", log)
-    .pipe(gulp.dest("./app/js"))
+    .pipe(gulp.dest("./dest/js", uglify()))
     .pipe(reload({stream: true}));
 });
 
+//сборка и вывод размера содержимого папки dest
+gulp.task("dest", ["jade", "css", "js", "images", "fonts", "extras"], function(){
+  return gulp.src("./dest/**/*").pipe(size({title:"Total build size"}));
+});
+
 // Собираем папку DEST (только после компиляции Jade, CSS, JS)
-gulp.task("build", ["clean", "jade", "css", "js"], function () {
+gulp.task("build", ["clean"], function() {
   gulp.start("dest");
 });
 
 // Запускаем локальный сервер (только после компиляции jade)
-gulp.task("server", ["jade", "css", "js"], function () {
+gulp.task("server", function () {
   browserSync({
     notify: false,
     port: 9000,
     server: {
-      baseDir: "./app"
+      baseDir: "./dest"
     }
   });
 });
 
 // слежка и запуск задач
 gulp.task("watch", function () {
-  gulp.watch("./app/templates/**/*.jade", ["jade"]).on("change", reload);
-  gulp.watch("./app/css/**/*.css", ["css"]).on("change", reload);
-  gulp.watch("./app/js/**/*.js", ["js"]).on("change", reload);
+  gulp.watch("./app/templates/**/*.jade", ["jade"]);
+  gulp.watch("./app/css/**/*.css", ["css"]);
+  gulp.watch("./app/js/**/*.js", ["js"]);
+  gulp.watch("./app/img/*", ["images"]);
 });
 
 // Задача по-умолчанию
-gulp.task("default", ["server", "watch"]);
+gulp.task("default", ["build", "server", "watch"]);
 
         //  =============
         //  == Функции ==
